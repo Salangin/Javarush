e8a723b8e2609248683292c60c3e3c568a1d2a82
Salangin <salangin@gmail.com>
2017-05-28 14:01:12 +0300

    2

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int result = scanner.nextInt();
+        if (result == 0) {
+            System.out.println("Result is 0");
+        } else {
+            System.out.println("Result is not 0");
+        }
+    }
+}
