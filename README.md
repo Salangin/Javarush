b48cfddca81ce7782470f86afd6b1eecfcd923f2
Salangin <salangin@gmail.com>
2017-05-28 21:16:05 +0300

    00.08. Level 0 is finished

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 1. 2016.xx.xx - level 00 - lecture 02
 1. 2016.xx.xx - level 00 - lecture 03
 1. 2016.xx.xx - level 00 - lecture 04
-
-
-
-
+1. 2016.xx.xx - level 00 - lecture 05
+1. 2016.xx.xx - level 00 - lecture 06
+1. 2016.xx.xx - level 00 - lecture 07
+1. 2016.xx.xx - level 00 - lecture 08
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,13 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    // comment
+    /*
+    00.06 comment
+    */
     public static void main(String[] args) {
         System.out.println("Hi Amigo"); // lesson 00.02
         int a; // lesson 00.04
+        System.out.println("Это не баг, это фича."); // 00.08
         Scanner scanner = new Scanner(System.in);
         int result = scanner.nextInt();
         if (result == 0) {
